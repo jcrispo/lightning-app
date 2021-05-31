6fd0d325272bfbfbd0dc937045d8eb998f12a932
Jason Crispo <jcrispo@iom.int>
2021-06-01 04:45:54 +0800

    initial commit

---
+++ src/main/java/com/dtn/app/LightningApp.java
@@ -0,0 +1,13 @@
+package com.dtn.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class LightningApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
